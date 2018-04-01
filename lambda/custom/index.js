c756257aac36ac634fbcc34eb388901ea245715e
Nicolas Sirimongkol <nsirimo@calstatela.edu>
2018-04-01 10:07:23 -0700

    Final Commit, for now.... Implemented Loggin into dynamodb and fixed small bugs

--- lambda/custom/index.js
+++ lambda/custom/index.js
@@ -1,58 +1,37 @@
 'use strict';
 const Alexa = require('alexa-sdk');
 var AWS = require('aws-sdk');
-var print = require('./helpers').printPretty;
-AWS.config.update({region:'us-east-1'});
 AWS.config.update({
     accessKeyId: "AKIAJJ7S3M23DBSECLGQ",
     secretAccessKey: "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",
     "region": "us-east-1"
 });
-var dynamodb = new AWS.DynamoDB();
+var dynamodb = new AWS.DynamoDB.DocumentClient();
 
-const APP_ID = undefined;
+const APP_ID = 'amzn1.ask.skill.acf2492c-e616-41ed-8c3e-1ab0f56fd6ca';
 
-const SKILL_NAME = 'Daily Doctor';
-const INITIAL_MESSAGE = "Is there any pain or discomfort?";
-const HELP_MESSAGE = 'You can say tell me a penis fact, or, you can say exit... What can I help you with?';
-const HELP_REPROMPT = 'What can I help you with?';
-const STOP_MESSAGE = 'Goodbye!';
+var date = " ";
+var user = "KenYang";
+var bodyPart = " ";
+var painDescription = " ";
+var painRating = " ";
+var additionalComments = " ";
 
-exports.handler = function(event, context, callback) {
-    var alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
-    var params = {
-        Item: {
-         "userId": {
-           S: "nicolassirimongkol",
-         },
-         "date": {
-             S: "2018-03-31T01:24:12Z"
-         },
-         "bodyPart": {
-            S:" penis"
-         },
-         "painRating": {
-             S: "2"
-         },
-         "additonalComments": {
-             S: "Sharp pain when farting."
-         }
-        }, 
-        ReturnConsumedCapacity: "TOTAL", 
-        TableName: "dailyDoctorLogs"
-       };
-       dynamodb.putItem(params, function(err, data) {
-         if (err) console.log(err, err.stack);
-         else     console.log(data);
-       });
-    alexa.registerHandlers(newSessionHandler, startGameHandlers, askQuestionHandlers, customHandlers, redoHandler, finishHandler);
-    alexa.execute();
-};
+
+function putItem(params) {
+    dynamodb.put(params, function (err, data) {
+        if (err) {
+            console.error("Unable to add item. Error JSON:", JSON.stringify(err, null, 2));
+        } else {
+            console.log("Added item:", JSON.stringify(data, null, 2));
+        }
+    });
+}
 
 // Launch the Alexa skill
 const newSessionHandler = {
     LaunchRequest() {
+        date = new Date();
         this.handler.state = "ASKMODE";
         this.emit(":ask", "Welcome to Daily Doctor. Are you feeling any pain or discomfort?");
     }
@@ -71,12 +50,14 @@ const startGameHandlers = Alexa.CreateStateHandler("ASKMODE", {
         this.emit(":ask", 'Please specify the area of discomfort or pain');
     },
     "AskRating": function () {
-      this.handler.state = "CUSTOMANSWERMODE";
-      this.emit(":ask", 'Please rate your discomfort on a scale from one to five, five being extreme pain and one being no pain.')
+        this.handler.state = "CUSTOMANSWERMODE";
+        this.emit(":ask", 'Please rate your discomfort on a scale from one to five, five being extreme pain and one being mild pain.')
     },
     "AdditionalDetails": function () {
+        var addDet = "I fart when I sneeze";
+        additionalComments = addDet;
         this.handler.state = "REDOMODE";
-        this.emit(":ask", 'Thank you, would you like to log any additional pain or discomfort?');
+        this.emit(":ask", 'Thank you. Would you like to log any additional pain or discomfort?');
     }
 });
 
@@ -93,10 +74,18 @@ const askQuestionHandlers = Alexa.CreateStateHandler("ANSWERMODE", {
 
 const customHandlers = Alexa.CreateStateHandler("CUSTOMANSWERMODE", {
     "logPain": function () {
+        var bodyPartValue = this.event.request.intent.slots.body_part.value;
+        bodyPart = bodyPartValue;
+        if ('value' in this.event.request.intent.slots.pain_description) {
+            var painDescriptionValue = this.event.request.intent.slots.pain_description.value;
+            painDescription = painDescriptionValue;
+        }
         this.handler.state = "ASKMODE";
         this.emitWithState("AskRating");
     },
     "ratePain": function () {
+        var painRatingValue = this.event.request.intent.slots.pain_rating.value;
+        painRating = painRatingValue;
         this.handler.state = "ASKMODE";
         this.emitWithState("AdditionalDetails");
     }
@@ -105,6 +94,7 @@ const customHandlers = Alexa.CreateStateHandler("CUSTOMANSWERMODE", {
 // Ask if there are any additional pain/discomforts
 const redoHandler = Alexa.CreateStateHandler("REDOMODE", {
     "AMAZON.YesIntent": function () {
+        date = new Date();
         this.handler.state = "ASKMODE";
         this.emitWithState("AskPain");
     },
@@ -116,7 +106,26 @@ const redoHandler = Alexa.CreateStateHandler("REDOMODE", {
 
 // Finish state
 const finishHandler = Alexa.CreateStateHandler("FINISHMODE", {
-   "FinishIntent": function() {
-       this.emit(":tell", "Thank you for logging your health. Have a good day!");
-   } 
+    "FinishIntent": function () {
+        this.emit(":tell", "Thank you for logging your health. Have a good day!");
+    }
 });
+
+exports.handler = function (event, context, callback) {
+    var alexa = Alexa.handler(event, context);
+    alexa.appId = APP_ID;
+    alexa.registerHandlers(newSessionHandler, startGameHandlers, askQuestionHandlers, customHandlers, redoHandler, finishHandler);
+    alexa.execute();
+    var params = {
+        TableName: "dailyDoctorLogs",
+        Item: {
+            "dateId": date.toString(),
+            "user": user.toString(),
+            "bodyPart": bodyPart.toString(),
+            "painDescription": painDescription.toString(),
+            "painRating": painRating.toString(),
+            "additionalComments": additionalComments.toString()
+        }
+    };
+    putItem(params);
+};
